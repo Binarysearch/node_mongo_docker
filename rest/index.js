b0fb80ac180bb3ce7aa66f4add2016dc84c4d9ef
Binarysearch <desenlace.net@gmail.com>
2019-06-24 21:45:03 +0200

    Api con express

--- rest/index.js
+++ rest/index.js
@@ -1,8 +1,26 @@
 // FileName: index.js
 // Import express
-let express = require('express')
+let express = require('express');
+
+
+let router = express.Router();
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+       status: 'API Its Working',
+       message: 'Welcome to RESTHub crafted with love!',
+    });
+});
+
+
+
+
+
 // Initialize the app
 let app = express();
+
+app.use('/api', router)
+
 // Setup server port
 var port = process.env.PORT || 8080;
 // Send message for default URL
